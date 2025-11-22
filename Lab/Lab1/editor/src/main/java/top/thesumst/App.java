d3f915cf0809b3d468a61b6b074985d45fd5a0ac
TheSumSt <2240765672@qq.com>
2025-11-23 03:20:35 +0800

    lab: create lab1 maven project

---
+++ Lab/Lab1/editor/src/main/java/top/thesumst/App.java
@@ -0,0 +1,13 @@
+package top.thesumst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
